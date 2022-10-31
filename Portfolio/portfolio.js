9a193f9e44627505ff7d03a73e37b9365f26ccee
waynehsu770 <waynehsu770@gmail.com>
2022-10-31 17:46:01 +0800

    update

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,9 @@
+// Contact Form Functions 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
